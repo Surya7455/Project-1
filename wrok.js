512f55d3f414449ef93e632ccb09a6de1d0ee38c
Surya7455 <114606367+Surya7455@users.noreply.github.com>
2024-01-01 02:52:22 +0530

    Add files via upload

---
+++ wrok.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("xsw").style.width = "200px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("xsw").style.width = "0";
+    document.getElementById("main").style.marginLeft= "80px";
+  }
+
+
